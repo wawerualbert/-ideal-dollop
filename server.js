9179cd54cce3549fc238e17fc826620cd2bb6f18
wawerualbert <50580926+wawerualbert@users.noreply.github.com>
2020-09-07 02:56:47 -0400

    Update server.js

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const path = require('path');
 const app = express();
 
 // Serve static files....
-app.use(express.static(__dirname + '/dist/recipe'));
+app.use(express.static(__dirname + '/dist/go-fullstack-activity'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/recipe/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/go-fullstack-activity/index.html'));
 });
 
 // default Heroku PORT
