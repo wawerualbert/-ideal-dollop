b5e5f65bc6b3efe4860c77f5f77e8e2c1e547119
wawerualbert58 <wawerualbert58@gmail.com>
2020-09-06 21:14:52 -0400

    changes to heroku

--- server.js
+++ server.js
@@ -2,13 +2,26 @@ const express = require('express');
 const path = require('path');
  
 const app = express();
+const forceSSL = function() {
+    return function (req, res, next) {
+      if (req.headers['x-forwarded-proto'] !== 'https') {
+        return res.redirect(
+         ['https://', req.get('Host'), req.url].join('')
+        );
+      }
+      next();
+    }
+  }// Instruct the app
+  // to use the forceSSL
+  // middleware
+app.use(forceSSL());
  
 // Serve only the static files form the angularapp directory
-app.use(express.static(__dirname + '/angularapp'));
+app.use(express.static(__dirname + '/dist/go-fullstack-activity'));
  
 app.get('/*', function(req,res) {
  
-res.sendFile(path.join(__dirname+'/angularapp/index.html'));
+res.sendFile(path.join(__dirname+'/dist/go-fullstack-activity/index.html'));
 });
  
 // Start the app by listening on the default Heroku port
